1dee8852cbb80b77fb5f3189a717cb6bd70b90ef
Dario Berzano <dario.berzano@gmail.com>
2019-03-26 15:51:05 +0100

    Test Travis

---
+++ run-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -ex
+
+set -o pipefail
+cd "$(dirname "$0")"
+
+if [[ $TRAVIS_PULL_REQUEST != false && $TRAVIS_COMMIT_RANGE ]]; then
+  # We are testing a Pull Request
+  DOCKER_ORG=alipierdev
+  #RUN_SETUP=1
+  #[[ $TRAVIS_PYTHON_VERSION == 3* ]] && RUN_DOCK=1 || RUN_DOCK=
+  #git diff --name-only $TRAVIS_COMMIT_RANGE | grep -q ^setup.py$ || RUN_SETUP=
+  #git diff --name-only $TRAVIS_COMMIT_RANGE | grep -q ^dock/     || RUN_DOCK=
+  #DOCKER_REPO=aliswdev
+elif [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == master ]]; then
+  # We are testing the master branch (e.g. when PR is merged)
+  DOCKER_ORG=alipier
+fi
+
+# Load Docker Hub user and password
+DOCKER_USER=$(eval echo \$DOCKER_USER_${DOCKER_ORG})
+DOCKER_PASS=$(eval echo \$DOCKER_PASS_${DOCKER_ORG})
+
+# Only rebuild containers that changed (TODO: now only works in PRs)
+git diff --name-only $TRAVIS_COMMIT_RANGE
+
+
+echo exit here just for fun
+false
+
+
+# Docker
+if [[ $RUN_DOCK ]]; then
+  docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+  pushd dock
+    docker build . -t "$DOCKER_REPO"/alidock:latest
+    docker tag "$DOCKER_REPO"/alidock:latest "$DOCKER_REPO"/alidock:cc7
+  popd
+  docker push "$DOCKER_REPO"/alidock:latest
+  docker push "$DOCKER_REPO"/alidock:cc7
+fi
