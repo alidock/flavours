e281e3e88ab755fe6429fccf19aae1101f129597
Dario Berzano <dario.berzano@gmail.com>
2019-03-26 16:53:04 +0100

    More tests

--- run-tests.sh
+++ run-tests.sh
@@ -4,16 +4,18 @@ set -o pipefail
 cd "$(dirname "$0")"
 
 if [[ $TRAVIS_PULL_REQUEST != false && $TRAVIS_COMMIT_RANGE ]]; then
-  # We are testing a Pull Request
-  DOCKER_ORG=alipierdev
+  # We are testing a Pull Request: do not push
+  DOCKER_ORG=
 elif [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == master ]]; then
   # We are testing the master branch (e.g. when PR is merged)
   DOCKER_ORG=alipier
 fi
 
 # Load Docker Hub user and password
-docker login -u "$(eval echo \$DOCKER_USER_${DOCKER_ORG})" \
-             -p "$(eval echo \$DOCKER_PASS_${DOCKER_ORG})"
+if [[ $DOCKER_ORG ]]; then
+  docker login -u "$(eval echo \$DOCKER_USER_${DOCKER_ORG})" \
+               -p "$(eval echo \$DOCKER_PASS_${DOCKER_ORG})"
+fi
 
 # Only rebuild containers that changed (TODO: now only works in PRs)
 while read DOCK; do
@@ -22,7 +24,9 @@ while read DOCK; do
   pushd "$DOCK"
     DOCKER_IMAGE="$DOCKER_ORG/${DOCK//\//:}"
     echo docker build . -t "$DOCKER_IMAGE"
-    echo docker push "$DOCKER_IMAGE"
+    if [[ $DOCKER_ORG ]]; then
+      echo docker push "$DOCKER_IMAGE"
+    fi
   popd
 done < <(git diff --name-only $TRAVIS_COMMIT_RANGE | grep / | cut -d/ -f1,2 | sort -u)
 
@@ -35,17 +39,5 @@ while read DOCK; do
   echo docker tag "$DOCKER_IMAGE_ORIG" "$DOCKER_IMAGE"
 done < <(git diff --name-only $TRAVIS_COMMIT_RANGE | grep / | cut -d/ -f1,2 | sort -u)
 
-echo exit here just for fun
+echo failing deliberately
 false
-
-
-# Docker
-if [[ $RUN_DOCK ]]; then
-  docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-  pushd dock
-    docker build . -t "$DOCKER_REPO"/alidock:latest
-    docker tag "$DOCKER_REPO"/alidock:latest "$DOCKER_REPO"/alidock:cc7
-  popd
-  docker push "$DOCKER_REPO"/alidock:latest
-  docker push "$DOCKER_REPO"/alidock:cc7
-fi
